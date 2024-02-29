ee1228ee31bdb7c0b0468bd3c4f9ba53bb3beeef
Ndukwe Onyinyechi <ljenclade@yahoo.com>
2024-02-29 19:09:30 +0000

    update

--- outputs.tf
+++ outputs.tf
@@ -10,9 +10,9 @@ output "subnet2_id" {
   value = aws_subnet.subnet2.id
 }
 
-#output "web_instance_public_ip" {
-# value = aws_instance.web.public_ip
-#}
+output "web_instance_public_ip" {
+value = aws_instance.web.public_ip
+}
 
 output "web_app_wait_command" {
   value       = "until curl --max-time 5 http://${aws_instance.web.public_ip} >/dev/null 2>&1; do echo preparing...; sleep 5; done; echo; echo -e 'Ready!!'"
